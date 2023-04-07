1c3c859f90bb5c1b4db4a4d14c36fc0b69dd10f6
Matheus Borges <matheus.borgest@gmail.com>
2023-04-07 18:43:14 +0100

    documentation of the BoardCell class

--- model/board_cell.py
+++ model/board_cell.py
@@ -1,4 +1,8 @@
 class BoardCell:
+    """
+    A class for the individual board cells, containing pointers
+    to the adjascent cells.
+    """
 
     def __init__(self, row, col, id):
         self.row = row
@@ -11,25 +15,35 @@ class BoardCell:
         self.right = None
 
     def __getitem__(self, key):
+        """
+        Used alongside the auxiliary attributes in the Klotski
+        and KlotskiState classes to get the adjascent cells.
+        
+        Args:
+            key (str): Referring to one of the four directions.
+
+        Returns:
+            BoardCell: The adjascent BoardCell referring to 
+            direction in the key attribute.
+        """
         return getattr(self, key)
     
     def late_init(self, board):
+        """
+        Setup of the pointers to the adjascent BoardCells.
+
+        Args:
+            board (BoardCell matrix): The board this BoardCell
+            is part of.
+        """
         if self.row - 1 >= 0:
             self.up = board[self.row-1][self.col]
-        else:
-            self.up = None
 
         if self.row + 1 <= len(board) - 1:
             self.down = board[self.row+1][self.col]
-        else:
-            self.down = None
 
         if self.col - 1 >= 0:
             self.left = board[self.row][self.col-1]
-        else:
-            self.left = None
 
         if self.col + 1 <= len(board[0]) - 1:
             self.right = board[self.row][self.col+1]
-        else:
-            self.right = None
